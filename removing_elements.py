c24bd3d33cd465e1f0545e01f8d70cc3ac2f8ff6
Jalani Paul <jalani2727@gmail.com>
2019-06-04 17:05:51 -0400

    completed with list comprehension

---
+++ removing_elements.py
@@ -0,0 +1,14 @@
+def remove_every_other(my_list):
+    # Your code here!
+    # just make a list of every even index
+
+    return [my_list[index] for index in range(0, len(my_list)) if index % 2 == 0]
+    
+   
+test.assert_equals(remove_every_other(['Hello', 'Goodbye', 'Hello Again']),
+                   ['Hello', 'Hello Again'])
+test.assert_equals(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+                   [1, 3, 5, 7, 9])
+test.assert_equals(remove_every_other([[1, 2]]), [[1, 2]])
+test.assert_equals(remove_every_other([['Goodbye'], {'Great': 'Job'}]),
+                   [['Goodbye']])
