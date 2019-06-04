ce2d22fdee24d765b82cf6d11785f320c75182e9
Jalani Paul <jalani2727@gmail.com>
2019-06-04 17:24:28 -0400

    success

--- removing_elements.py
+++ removing_elements.py
@@ -11,9 +11,15 @@ def remove_every_other(my_list):
     # just make a list of every even index
     return [my_list[index] for index in range(0, len(my_list)) if index % 2 == 0]
     
-class MyTest (unittest.TestCase):
-    def test(self):
+class MyTests (unittest.TestCase):
+    def test1(self):
         self.assertEqual(remove_every_other(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again'])
+    def test2(self):
         self.assertEqual(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [1, 3, 5, 7, 9])
+    def test3(self):
         self.assertEqual(remove_every_other([[1, 2]]), [[1, 2]])
+    def test4(self):
         self.assertEqual(remove_every_other([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']])
+
+if __name__ == '__main__':
+        unittest.main()
